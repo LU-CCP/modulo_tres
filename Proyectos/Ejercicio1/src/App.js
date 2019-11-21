0f39fc7f5f26eaaf74c45183f0146fa7f8f37c5f
ivettef <56972785+ivettef@users.noreply.github.com>
2019-11-21 17:35:36 -0300

    fin ejercicio1

--- Proyectos/Ejercicio1/src/App.js
+++ Proyectos/Ejercicio1/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Textinput, Button} from './components'
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      textinputDisabled:false
     };
-  }
+  }  
+   handleClick = () =>
+    {
+      const {textinputDisabled} = this.state;
+      this.setState({textinputDisabled:!textinputDisabled})
+    }
   render(){
-    
+    const {textinputDisabled} = this.state;
     return (
     <div className="App">
+      <Textinput disabled ={textinputDisabled}/>
+      <Button onClick = {this.handleClick}/>
     </div>
   );
   }
