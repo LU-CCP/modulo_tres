5279fa50424b5b45ddef6f18e7532fb8a53eab46
bryanm-lagash <bryanm@lagash.com>
2019-11-21 18:28:19 -0300

    Forms and Events

---
+++ Proyectos/eventos/src/components/index.js
@@ -0,0 +1,6 @@
+import TextInput from './TextInput.js'
+import Button from './Button.js'
+
+export {
+    TextInput, Button
+}
---
+++ Proyectos/formularios/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: ''
+    };
+  }
+
+  handleChange = (key) => ({ target: { value }}) => {
+    console.log(key, this.state, value)
+    this.setState({ [key]: value });
+  }
+
+  handleSubmit = () => {
+    const {firstName, lastName, email} = this.state;
+    alert(`${firstName} ${lastName} ${email}`)
+  }
+
+  render(){
+    const {firstName, lastName, email} = this.state;
+    return (
+      <div className="App">
+        <form className="Form" onSubmit={this.handleSubmit}>
+          <input required value={firstName} onChange={this.handleChange('firstName')} ></input>
+          <input required value={lastName} onChange={this.handleChange('lastName')}></input>
+          <input required type='email' value={email} onChange={this.handleChange('email')}></input>
+          <button type='submit'>{'Submit'}</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
