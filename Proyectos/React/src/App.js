5e2658c090ac7ce536871b21fa75f0cac1f24139
alanb-lagash <56938450+alanb-lagash@users.noreply.github.com>
2019-11-21 17:37:57 -0300

    proyecto de componentes

--- Proyectos/React/src/App.js
+++ Proyectos/React/src/App.js
@@ -1,59 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TextInput,Button} from './components';
 
 class App extends React.Component {
   constructor(props){
     super (props);
     this.state = {
-      value: 'Learn React'
+      TextInputDisabled: false
     };
-    this._varGlobal = '';
-    console.log('constructor')
   }
-  shouldComponentUpdate(nextProps, nextState)
-  {
-    const {value, compare} = this.state;
-    const should = nextState.value !== value || nextState.compare !== compare;
-    return should;
-  }
-  componentDidMount(){
-    console.log('componentDidMount')
-    setTimeout(() => {
-      this.setState({value: 'learn React Now!'});
-    }, 3000)
-    setTimeout(() => {
-      this._varGlobal = 'please';
-      console.log('forceUpdate')
-      this.forceUpdate ();
-    }, 4000)
-    setTimeout(() => {
-      console.log('setState compare')
-      this.setState({compare: ''});
-    }, 5000)
-
-  }
-  componentDidUpdate() {
-    console.log('componentDidUpdate')
+ 
+  handleClick = () => {
+    const {TextInputDisabled}= this.state;
+    this.setState({ TextInputDisabled: !TextInputDisabled});
   }
   render (){
-    console.log('render')
+    const {TextInputDisabled}=this.state;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {`${this.state.value} ${this._varGlobal}`}
-        </a>
-      </header>
+    <div className="App"> 
+      <TextInput disabled={TextInputDisabled}/>
+      <Button onClick={this.handleClick}/>
     </div>
   );
 }
