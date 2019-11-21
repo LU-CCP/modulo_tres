3137dc5b32cdbffa117558275252c17a7e6b9dbb
fabianb-lagash <fabianb@lagash.com>
2019-11-21 18:26:30 -0300

    Se actualiza proyecto eventos

--- Proyectos/FabianB/Eventos/src/App.js
+++ Proyectos/FabianB/Eventos/src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import './App.css';
+import { Textinput, Button } from './components';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      textInputDisable: false
     };
-     
+
 
   }
+  handleClick = () => {
+    const { textInputDisable } = this.state;
+    this.setState({ textInputDisable: !textInputDisable });
+
+  }
+
   render() {
     console.log('render')
+    const { textInputDisable } = this.state;
     return (
+
       <div className="App">
-  
+        <Textinput disabled={textInputDisable} />
+        <Button onClick={this.handleClick} />
+
       </div>
     );
   }
