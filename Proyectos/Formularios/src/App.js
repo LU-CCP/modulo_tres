899f99c2cbe4a8a6e1392fa7885f671d9363973f
ivettef <56972785+ivettef@users.noreply.github.com>
2019-11-21 17:55:42 -0300

    comienza a armar estilos

--- Proyectos/Formularios/src/App.js
+++ Proyectos/Formularios/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      firstName: '',
+      lastName:''
     };
   }
   render(){
-    
+    const {firstName, lastName} = this.state;
     return (
     <div className="App">
+      <form className = 'Form'>
+      <input value = {firstName}/>
+      <input value = {lastName}/>
+      </form>
     </div>
   );
   }
