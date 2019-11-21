a4b5467bd3fde93553fbad0bfaa01ea511651094
maximilianogom-lagash <57406898+maximilianogom-lagash@users.noreply.github.com>
2019-11-21 18:26:50 -0300

    Se agrega Carpeta de Proyecto 1 y Formulario

---
+++ Proyectos/Modulo 3 proyecto 1/src/Components/HiperLink.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Hiperlink extends React.Component{
+    render(){
+        return(
+            <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Maximiliano
+          </a>
+        )
+    }
+}
+
+export default Hiperlink;
