ad20684723a7abcd29d9772971d99432dbf4933b
Alfredo Puerta <56939672+alfredop-lagash@users.noreply.github.com>
2019-11-22 15:42:08 -0300

    Tarea culminada completamente

---
+++ Proyectos/EjercicioFormulario/src/App.js
@@ -0,0 +1,82 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      mail: "",
+      date: "",
+      gender: ["masculino", "femenino"]
+    };
+  }
+
+  handleChange = key => ({ target: { value } }) => {
+    console.log(key, this.state, value);
+    this.setState({ [key]: value });
+  };
+
+  handleSubmit = () => {
+    const { firstName, lastName, mail, date, gender } = this.state;
+    alert(`${firstName} ${lastName} ${mail} ${date} ${gender}`);
+  };
+
+  render() {
+    const { firstName, lastName, mail, date, gender } = this.state;
+    return (
+      <div className="App">
+        <form className="form" onSubmit={this.handleSubmit}>
+          <input
+            required
+            value={firstName}
+            onChange={this.handleChange("firstName")}
+          />
+          <input
+            required
+            value={lastName}
+            onChange={this.handleChange("lastName")}
+          />
+          <input
+            required
+            type="email"
+            value={mail}
+            onChange={this.handleChange("mail")}
+          />
+          <input
+            required
+            type="date"
+            value={date}
+            onChange={this.handleChange("date")}
+          />
+          <label>
+            <input
+              type="radio"
+              id="masculino"
+              name="gender"
+              value={gender[0]}
+              onChange={this.handleChange("gender")}
+            />
+            Masculino
+          </label>
+          <label>
+            <input
+              type="radio"
+              id="femenino"
+              name="gender"
+              value={gender[1]}
+              onChange={this.handleChange("gender")}
+            />
+            Femenino
+          </label>
+          <button type="submit">
+            {"Submit"}
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
