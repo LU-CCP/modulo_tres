9a6136d8a6d9b24682e1cafe915d0981db3505c3
bryanm-lagash <bryanm@lagash.com>
2019-11-22 17:55:43 -0300

    Funciones nativas de JavaScript

--- Proyectos/BryanM/ejercicio1/src/App.js
+++ Proyectos/BryanM/ejercicio1/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+const genders = ['male','female','others'];
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -22,23 +25,47 @@ class App extends Component {
     alert(`${firstName} ${lastName} ${email} ${gender} ${birthdate}`)
   }
 
+  renderGendersFor = () => {
+    const gendersJSX = [];
+    for (let i = 0; i < genders.length; i++) {
+      const gender = genders[i];
+      gendersJSX.push(
+      <div key={gender} >
+        <input className='App-Form-Item' name='genders' required type='radio' value={gender}onChange={this.handleChange('gender')} />{gender}
+      </div> );
+    }
+    return gendersJSX;
+  }
+
+  renderGenderForEach = () => {
+    const gendersJSX = [];
+    genders.forEach((gender) => {
+      gendersJSX.push(
+        <div key={gender} >
+          <input className='App-Form-Item' name='genders' required type='radio' value={gender}onChange={this.handleChange('gender')} />{gender}
+        </div> );
+    });
+    return gendersJSX;
+  }
+
+  renderGenderMap = () => {
+    return genders.map((gender) => {
+      return (
+        <div key={gender} >
+          <input className='App-Form-Item' name='genders' required type='radio' value={gender}onChange={this.handleChange('gender')} />{gender}
+        </div> )
+    })
+  }
+
   render() {
-    const { firstName, lastName, email, gender, birthdate } = this.state;
+    const { firstName, lastName, email, birthdate } = this.state;
     return (
       <div className='App'>
         <form className='App-Form' onSubmit={this.handleSubmit}>
           <input className='App-Form-Item' required value={firstName} onChange={this.handleChange('firstName')} />
           <input className='App-Form-Item' required value={lastName} onChange={this.handleChange('lastName')} />
           <input className='App-Form-Item' required type='email' value={email} onChange={this.handleChange('email')} />
-          <div>
-            <input className='App-Form-Item' name='genders' required type='radio' value='male'onChange={this.handleChange('gender')} />Male
-          </div>
-          <div>
-            <input className='App-Form-Item' name='genders' required type='radio' value='female' onChange={this.handleChange('gender')} />Female
-          </div>
-          <div>
-            <input className='App-Form-Item' name='genders' required type='radio' value='other' onChange={this.handleChange('gender')} />Other
-          </div>
+          {this.renderGenderMap()}
           <input className='App-Form-Item' required type='date' value={birthdate} onChange={this.handleChange('birthdate')} />
           <button className='App-Form-Item' type='submit'>{'Submit'}</button>
         </form>
