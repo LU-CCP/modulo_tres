a4ff53ae05acc3f29a0b3d51beef9c08e2540232
nicolasar-lagash <nicolasar@lagash.com>
2019-11-22 13:49:50 -0300

    Tarea terminada

--- Proyectos/NicolasArias/formularios-tarea/src/App.js
+++ Proyectos/NicolasArias/formularios-tarea/src/App.js
@@ -7,7 +7,9 @@ class App extends React.Component {
     this.state = {
       firstName: '',
       lastName: '',
-      email: ''
+      email: '',
+      gender: '',
+      date: ''
     }
   }
 
@@ -16,21 +18,30 @@ class App extends React.Component {
   }
 
   handleSubmit = () =>{
-    const { firstName, lastName, email } = this.state;
-    alert(`${firstName} ${lastName} ${email}`)
+    const { firstName, lastName, email, gender, date } = this.state;
+    alert(` Nombre: ${firstName}\n
+            Apellido: ${lastName}\n
+            Correo: ${email}\n
+            Género: ${gender}\n
+            Fecha: ${date}`)
   }
 
   render(){
-    const { firstName, lastName, email } = this.state;
+    const { firstName, lastName, gender, email, date } = this.state;
+  
     return(
-      <div className="App">
+      <div className="Form">
         <form className="Form" onSubmit={this.handleSubmit}>
-          <input required value={firstName} onChange={this.handleChange('firstName')} />
-          <input required value={lastName} onChange={this.handleChange('lastName')} />
-          <input required type='email' value={email} onChange={this.handleChange('email')} />
+          <input required placeholder='Nombre' value={firstName} onChange={this.handleChange('firstName')} />
+          <input required placeholder='Apellido' value={lastName} onChange={this.handleChange('lastName')} />
+          <input required placeholder='Email' type='email' value={email} onChange={this.handleChange('email')} />
+          <input required type="radio" name='genero' value='masculino' checked={this.state.genero==='masculino'} onChange={this.handleChange('gender')} /> Masculino
+          <input required type="radio" name='genero' value='femenino' checked={this.state.genero==='femenino'} onChange={this.handleChange('gender')}/> Femenino
+          <input required type='date' value={date} onChange={this.handleChange('date')} />
           <button type='submit'> {'Submit'} </button>
         </form>
       </div>
+
     );
   }
 }
