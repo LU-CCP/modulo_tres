b7aee7808cfad6482fd19d3836df54e94a38af32
nicolasar-lagash <nicolasar@lagash.com>
2019-11-22 15:55:03 -0300

    Tarea arreglada y terminada
    Codigo extraído de un compañero para poder resolver el problema de los radio button

--- Proyectos/NicolasArias/formularios-tarea/src/App.js
+++ Proyectos/NicolasArias/formularios-tarea/src/App.js
@@ -1,48 +1,44 @@
 import React from 'react';
 import './App.css';
 
+
 class App extends React.Component {
   constructor(props){
     super(props);
-    this.state = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      gender: '',
-      date: ''
-    }
+    this.state ={
+      nombre:'',
+      apellido:'',
+      email:'',
+      genero:'',
+      date:''
+    };
   }
+  handleChange=(key)=>({target:{value}})=>{
+    console.log(key, this.state, value)
+    this.setState({ [key] :value });
 
-  handleChange = (key) => ({target: {value}}) =>{
-    this.setState({ [key]:value });
   }
-
-  handleSubmit = () =>{
-    const { firstName, lastName, email, gender, date } = this.state;
-    alert(` Nombre: ${firstName}\n
-            Apellido: ${lastName}\n
-            Correo: ${email}\n
-            Género: ${gender}\n
-            Fecha: ${date}`)
-  }
-
-  render(){
-    const { firstName, lastName, gender, email, date } = this.state;
   
-    return(
-      <div className="Form">
-        <form className="Form" onSubmit={this.handleSubmit}>
-          <input required placeholder='Nombre' value={firstName} onChange={this.handleChange('firstName')} />
-          <input required placeholder='Apellido' value={lastName} onChange={this.handleChange('lastName')} />
-          <input required placeholder='Email' type='email' value={email} onChange={this.handleChange('email')} />
-          <input required type="radio" name='genero' value='masculino' checked={this.state.genero==='masculino'} onChange={this.handleChange('gender')} /> Masculino
-          <input required type="radio" name='genero' value='femenino' checked={this.state.genero==='femenino'} onChange={this.handleChange('gender')}/> Femenino
-          <input required type='date' value={date} onChange={this.handleChange('date')} />
-          <button type='submit'> {'Submit'} </button>
-        </form>
-      </div>
+  handleSubmit = ()=>{
+    const {nombre,apellido, email, genero, date}= this.state;
+    alert(`${nombre} ${apellido} ${email} ${genero} ${date}`)
+  }
+  render(){
+    const {nombre,apellido,email,genero,date}= this.state;
 
-    );
+    return(
+        <div className="App">
+          <form className="Form" onSubmit={this.handleSubmit}>
+            <input required value={nombre} onChange={this.handleChange('nombre')}/>
+            <input required value={apellido} onChange={this.handleChange('apellido')}/>
+            <input required type="email" value={email} onChange={this.handleChange('email')}/>
+            <input required type="radio" name='genero' value='masculino' checked={this.state.genero==='masculino'} onChange={this.handleChange('genero')} /> Masculino
+            <input required type="radio" name='genero' value='femenino' checked={this.state.genero==='femenino'} onChange={this.handleChange('genero')}/> Femenino
+            <input required type="date" value={date} onChange={this.handleChange('date')}/>
+            <button type="submit">Guardar</button>
+          </form>
+        </div>
+      );
   }
 }
 
