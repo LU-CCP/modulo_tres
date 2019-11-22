a2d360664f57227da1e0ff712766c64868acfad2
marcoac001 <marcoac001@lagash.com>
2019-11-22 16:13:57 -0300

    Arregle el ejercicio 1 con lo pedido

--- Proyectos/MarcoA/Ejercicio1/src/App.js
+++ Proyectos/MarcoA/Ejercicio1/src/App.js
@@ -7,7 +7,9 @@ class App extends React.Component {
     this.state = {
       firstName: '',
       lastName: '',
-      email: ''
+      email: '',
+      genero: '',
+      fechaNacimiento: ''
     };
   }
 
@@ -17,19 +19,22 @@ class App extends React.Component {
   }
 
   handleSubmit = () => {
-    const { firstName, lastName, email } = this.state;
-    alert(`${firstName} ${lastName} ${email}`)
+    const { firstName, lastName, email, genero, fechaNacimiento } = this.state;
+    alert(`${firstName} ${lastName} ${email} ${genero} ${fechaNacimiento}`)
   }
 
 
   render() {
-    const { firstName, lastName, email } = this.state;
+    const { firstName, lastName, email, fechaNacimiento } = this.state;
     return (
       <div className="App">
         <form className="Form" onSubmit={this.handleSubmit}>
-          Formulario Lagash<input required value={firstName} onChange={this.handleChange('firstName')} />
-          <input required value={lastName} onChange={this.handleChange('lastName')} />
-          <input required type='email' value={email} onChange={this.handleChange('email')} />
+          Nombre<input required value={firstName} onChange={this.handleChange('firstName')} />
+          Apellido<input required value={lastName} onChange={this.handleChange('lastName')} />
+          Correo<input required type='email' value={email} onChange={this.handleChange('email')} />
+          Genero<input required type='radio' name='genero' value='Masculino' checked={this.state.genero === 'Masculino'} onChange={this.handleChange('genero')} /> Masculino
+          <input required type='radio' name='genero' value='Femenino' checked={this.state.genero === 'Femenino'} onChange={this.handleChange('genero')} /> Femenino <br></br>
+          Fecha de nacimiento<input required type='date' value={fechaNacimiento} onChange={this.handleChange('fechaNacimiento')} />
           <button type='submit'>{'Subir'}</button>
         </form>
       </div>
