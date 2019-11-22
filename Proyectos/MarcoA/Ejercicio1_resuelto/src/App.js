e9bfc3f5d6ebca5c0b37739c866f275fe9e20a8e
marcoac001 <marcoac001@lagash.com>
2019-11-22 17:55:34 -0300

    Se agregan las funciones trabajadas en clases

--- Proyectos/MarcoA/Ejercicio1_resuelto/src/App.js
+++ Proyectos/MarcoA/Ejercicio1_resuelto/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const generos = ['Hombre', 'Mujer', 'Helicoptero Apache'];
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -13,7 +15,58 @@ class App extends Component {
     };
   }
 
-  handleChange = (key) => ({ target: { value }}) => {
+  renderGenerosForEach = () => {
+    const generosJSX = [];
+    generos.forEach((genero) => {
+      generosJSX.push(
+        <div key={genero}>
+          <input className='App-Form-Item'
+            name='genero'
+            required
+            type='radio'
+            value={genero}
+            onChange={this.handleChange('genero')}
+          />{genero}
+        </div>)
+    });
+    return generosJSX;
+  }
+
+  renderGenerosFor = () => {
+    const generosJSX = [];
+    for (let i = 0; i < generos.length; i++) {
+      const genero = generos[i];
+      generosJSX.push(
+        <div key={genero}>
+          <input className='App-Form-Item'
+
+            name='genero'
+            required
+            type='radio'
+            value={genero}
+            onChange={this.handleChange('genero')}
+          />{genero}
+        </div>)
+    }
+    return generosJSX;
+  }
+
+  renderGenerosMap = () => {
+    return generos.map((genero) => {
+      return (
+        <div key={genero}>
+          <input className='App-Form-Item'
+            name='genero'
+            required
+            type='radio'
+            value={genero}
+            onChange={this.handleChange('genero')}
+          />{genero}
+        </div>);
+    })
+  }
+
+  handleChange = (key) => ({ target: { value } }) => {
     this.setState({ [key]: value });
   }
 
@@ -23,24 +76,16 @@ class App extends Component {
   }
 
   render() {
-    const { firstName, lastName, email, gender, birthdate } = this.state;
+    const { firstName, lastName, email, birthdate } = this.state;
     return (
       <div className='App'>
         <form className='App-Form' onSubmit={this.handleSubmit}>
-          <input className='App-Form-Item' required value={firstName} onChange={this.handleChange('firstName')} />
-          <input className='App-Form-Item' required value={lastName} onChange={this.handleChange('lastName')} />
-          <input className='App-Form-Item' required type='email' value={email} onChange={this.handleChange('email')} />
-          <div>
-            <input className='App-Form-Item' name='genders' required type='radio' value='male'onChange={this.handleChange('gender')} />Male
-          </div>
-          <div>
-            <input className='App-Form-Item' name='genders' required type='radio' value='female' onChange={this.handleChange('gender')} />Female
-          </div>
-          <div>
-            <input className='App-Form-Item' name='genders' required type='radio' value='other' onChange={this.handleChange('gender')} />Other
-          </div>
-          <input className='App-Form-Item' required type='date' value={birthdate} onChange={this.handleChange('birthdate')} />
-          <button className='App-Form-Item' type='submit'>{'Submit'}</button>
+        Nombre <input className='App-Form-Item' required value={firstName} onChange={this.handleChange('firstName')} />
+        Apellido    <input className='App-Form-Item' required value={lastName} onChange={this.handleChange('lastName')} /> 
+        Correo    <input className='App-Form-Item' required type='email' value={email} onChange={this.handleChange('email')} />
+          Genero {this.renderGenerosMap()}
+          Cumpleaños <input className='App-Form-Item' required type='date' value={birthdate} onChange={this.handleChange('birthdate')} />
+            <button className='App-Form-Item' type='submit'>{'Submit'}</button>
         </form>
       </div>
     );
