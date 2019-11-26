5e6b4cbb6c67dd8d91c7eda57a03c8bd22d1cf4c
marcoac001 <marcoac001@lagash.com>
2019-11-26 18:02:34 -0300

    Se agrega la solucion en Lint de Componentes-Funcionales

--- Proyectos/MarcoA/Componentes-Funcionales/src/App.js
+++ Proyectos/MarcoA/Componentes-Funcionales/src/App.js
@@ -8,59 +8,67 @@ class App extends Component {
     super(props);
     this.state = {
       firstName: '',
+
       lastName: '',
+
       email: '',
+
       gender: null,
+
       birthdate: ''
     };
   }
 
-
   renderGendersFor = () => {
     const gendersJSX = [];
-    for (let i = 0; i < genders.length; i++) {
+
+    for (let i = 0; i < genders.length; i += 1) {
       const gender = genders[i];
+
       gendersJSX.push(
         <div key={gender}>
-          <input 
-            className='App-Form-Item' 
-            name='genders' 
-            required 
-            type='radio' 
+          <input
+            className='App-Form-Item'
+            name='genders'
+            required
+            type='radio'
             value={gender}
-            onChange={this.handleChange('gender')} 
-            />{gender}
+            onChange={this.handleChange('gender')}
+          />
+          {gender}
         </div>
       );
     }
+
     return gendersJSX;
-  }
+  };
 
   renderGendersForEach = () => {
     const gendersJSX = [];
-    genders.forEach((gender) => {
+
+    genders.forEach(gender => {
       gendersJSX.push(
         <div key={gender}>
-          <input 
-            className='App-Form-Item' 
-            name='genders' 
-            required 
-            type='radio' 
+          <input
+            className='App-Form-Item'
+            name='genders'
+            required
+            type='radio'
             value={gender}
-            onChange={this.handleChange('gender')} 
-            />{gender}
+            onChange={this.handleChange('gender')}
+          />
+          {gender}
         </div>
       );
     });
+
     return gendersJSX;
-  }
-
-
+  };
 
   render() {
     const { firstName, lastName, email, birthdate } = this.state;
-    return ( null
-    );
+
+    return null;
   }
 }
 
--- Proyectos/MarcoA/Componentes-Funcionales/src/components/Functions.js
+++ Proyectos/MarcoA/Componentes-Funcionales/src/components/Functions.js
@@ -1,55 +1,76 @@
-
 import React, { Component } from 'react';
 
-const geners = ['male', 'female', other];
+const genders = ['male', 'female', other];
 
 const Function = () => {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [birthdate, setBirthdate] = useState('');
-    const [email, setEmail] = useState('');
-    const [gender, setGender] = useState('');
-    const [email, setEmail] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [birthdate, setBirthdate] = useState('');
+  const [gender, setGender] = useState('');
+  const [email, setEmail] = useState('');
 
-
-    const handleChange = (key) => ({ target: { value } }) => {
-        switch(key){
-            
-        }
-        setState({ [key]: value });
+  const handleChange = key => ({ target: { value } }) => {
+    switch (key) {
     }
 
-    const handleSubmit = () => {
-        const { firstName, lastName, email, gender, birthdate } = state;
-        alert(`${firstName} ${lastName} ${email} ${gender} ${birthdate}`)
-    }
+    setState({ [key]: value });
+  };
 
-    const renderGendersMap = () => {
-        return genders.map((gender) => {
-            return (
-                <div key={gender}>
-                    <input
-                        className='App-Form-Item'
-                        name='genders'
-                        required
-                        type='radio'
-                        value={gender}
-                        onChange={handleChange('gender')}
-                    />{gender}
-                </div>
-            );
-        })
-    }
+  const handleSubmit = () => {
+    const { firstName, lastName, email, gender, birthdate } = state;
 
-    return (
-        <div className='App'>
-            <form className='App-Form' onSubmit={handleSubmit}>
-                <input className='App-Form-Item' required value={firstName} onChange={handleChange('firstName')} />
-                <input className='App-Form-Item' required value={lastName} onChange={handleChange('lastName')} />
-                <input className='App-Form-Item' required type='email' value={email} onChange={handleChange('email')} />
-                {renderGendersMap()}
-                <input className='App-Form-Item' required type='date' value={birthdate} onChange={handleChange('birthdate')} />
-                <button className='App-Form-Item' type='submit'>{'Submit'}</button>
-            </form>
-        </div>)
-}
+    alert(`${firstName} ${lastName} ${email} ${gender} ${birthdate}`);
+  };
+
+  const renderGendersMap = () =>
+    genders.map(gender => (
+      <div key={gender}>
+        <input
+          className='App-Form-Item'
+          name='genders'
+          required
+          type='radio'
+          value={gender}
+          onChange={handleChange('gender')}
+        />
+        {gender}
+      </div>
+    ));
+
+  return (
+    <div className='App'>
+      <form className='App-Form' onSubmit={handleSubmit}>
+        <input
+          className='App-Form-Item'
+          required
+          value={firstName}
+          onChange={handleChange('firstName')}
+        />
+        <input
+          className='App-Form-Item'
+          required
+          value={lastName}
+          onChange={handleChange('lastName')}
+        />
+        <input
+          className='App-Form-Item'
+          required
+          type='email'
+          value={email}
+          onChange={handleChange('email')}
+        />
+        {renderGendersMap()}
+        <input
+          className='App-Form-Item'
+          required
+          type='date'
+          value={birthdate}
+          onChange={handleChange('birthdate')}
+        />
+        <button className='App-Form-Item' type='submit'>
+          {'Submit'}
+        </button>
+      </form>
+    </div>
+  );
+};
