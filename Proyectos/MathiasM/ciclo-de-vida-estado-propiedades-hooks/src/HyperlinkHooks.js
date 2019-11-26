ecbc9164f3b83da1459d3fe23c79439591f1a077
mathiasm-lagash <56935943+mathiasm1@users.noreply.github.com>
2019-11-26 18:08:33 -0300

    corregido ejercicio ciclo-vida-estado-propiedades-hooks con lint

--- Proyectos/MathiasM/ciclo-de-vida-estado-propiedades-hooks/src/HyperlinkHooks.js
+++ Proyectos/MathiasM/ciclo-de-vida-estado-propiedades-hooks/src/HyperlinkHooks.js
@@ -1,39 +1,38 @@
-import React, { useRef,useState,useEffect,memo } from 'react';
+import React, { useRef, useState, useEffect, memo } from 'react';
 
-const Hyperlink =(props)=> {
+const Hyperlink = props => {
   const _varGlobalRef = useRef('');
   const [value, setValue] = useState('Learn React');
   const [compare, setCompare] = useState('');
   const [showInstructions, setShowInstructions] = useState(true);
 
-  useEffect(()=>{
-    if(showInstructions !== props.showInstructions){
-      setShowInstructions(props.showInstructions)
+  useEffect(() => {
+    if (showInstructions !== props.showInstructions) {
+      setShowInstructions(props.showInstructions);
     }
-  },[props.showInstructions]);
+  }, [props.showInstructions, showInstructions]);
 
-  useEffect(()=>{
-    console.log('Hyperlink componentDidMount')
+  useEffect(() => {
+    console.log('Hyperlink componentDidMount');
     setTimeout(() => {
-      console.log('Hyperlink setState')
+      console.log('Hyperlink setState');
       setValue('Learn React Now!');
-    }, 3000)
+    }, 3000);
     setTimeout(() => {
       _varGlobalRef.current = 'Please';
-    }, 4000)
+    }, 4000);
     setTimeout(() => {
-      console.log('Hyperlink setState compare')
+      console.log('Hyperlink setState compare');
       setCompare('');
-      const { onHide }  = props;
+      const { onHide } = props;
+
       onHide();
-    }, 5000)
+    }, 5000);
+  }, [props]);
 
-  },[]);
-
-  useEffect(()=>{
-    console.log('Hyperlink componentDidUpdate')
+  useEffect(() => {
+    console.log('Hyperlink componentDidUpdate');
   });
- 
-}
+};
 
 export default memo(Hyperlink);
