67bd0b28fba4ab5cee1ceef7f7f5161f55fb3bd0
jeremya-lagash <jeremya@lagash.com>
2019-11-26 17:02:29 -0300

    instalacion de dependencias eslint prettier y husky listas

--- Proyectos/jeremy_andrades/ciclo-de-vida/src/AppHook.js
+++ Proyectos/jeremy_andrades/ciclo-de-vida/src/AppHook.js
@@ -1,55 +1,55 @@
-import React, { useEffect, useState, useRef, memo } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef, memo } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 // const arr = [1,2];
 // const [uno, dos] = arr;
 
 const App = () => {
-    const _varGlobalRef = useRef('')
-    const [value, setValue] = useState('Learn React')
-    const [compare, setCompare] = useState('')
+  const _varGlobalRef = useRef("");
+  const [value, setValue] = useState("Learn React");
+  const [compare, setCompare] = useState("");
 
-    //componentDidMount
-    useEffect(() => {
-        console.log('componentDidMount')
+  //componentDidMount
+  useEffect(() => {
+    console.log("componentDidMount");
     setTimeout(() => {
-      console.log('setState')
-      setValue('Learn React Now!');
-    }, 3000)
+      console.log("setState");
+      setValue("Learn React Now!");
+    }, 3000);
     setTimeout(() => {
-      _varGlobalRef.current = 'Please';
-    }, 4000)
+      _varGlobalRef.current = "Please";
+    }, 4000);
     setTimeout(() => {
-      console.log('setState compare')
-      setCompare('');
-    }, 5000)
-    }, [])
+      console.log("setState compare");
+      setCompare("");
+    }, 5000);
+  }, []);
 
-    // componentDidUpdate solo cuando value cambia
-    useEffect(() => {
-        console.log('componentDidUpdate value', value)
-    }, [value])
+  // componentDidUpdate solo cuando value cambia
+  useEffect(() => {
+    console.log("componentDidUpdate value", value);
+  }, [value]);
 
-    return(
-        <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {`${value} ${_varGlobalRef.current}`}
-          </a>
-        </header>
-      </div>
-    )
-}
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {`${value} ${_varGlobalRef.current}`}
+        </a>
+      </header>
+    </div>
+  );
+};
 
 // equivalent a shouldComponentUpdate
 export default memo(App);
