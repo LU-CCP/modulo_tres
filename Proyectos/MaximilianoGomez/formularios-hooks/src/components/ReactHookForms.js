cae679be6cbaf68a6d751f941daaf2f42b58ddce
maximilianogom-lagash <57406898+maximilianogom-lagash@users.noreply.github.com>
2019-11-26 15:42:41 -0300

    Actualizacion

---
+++ Proyectos/MaximilianoGomez/formularios-hooks/src/components/ReactHookForms.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import useForm from 'react-hook-form'
+
+import"../App.css";
+const genders = ["male", "female", "other"];
+
+
+const Function = () => {
+
+    const { register, handleSubmit } = useForm()
+
+    const onSubmit = (data) => {
+        const { firstName, lastName, email, birthdate } = data;
+        alert(`${firstName} ${lastName} ${email} ${birthdate}`);
+    }
+
+    return (
+        <div className='App'>
+          <form className='App-Form' onSubmit={handleSubmit(onSubmit)}>
+            <input className='App-Form-Item' required name='firstName' ref={register} />
+            <input className='App-Form-Item' required name='lastName' ref={register} />
+            <input className='App-Form-Item' required type='email' name='email' ref={register} />
+            <input className='App-Form-Item' required type='date' name='birthdate' ref={register}/>
+            <button className='App-Form-Item' type='submit'>{'Submit'}</button>
+          </form>
+        </div>
+      );
+}
+
+export default Function;
