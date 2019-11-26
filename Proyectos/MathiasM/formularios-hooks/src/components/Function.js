0f29d68e1e523f2e4492e2314612bbe6a921331f
mathiasm-lagash <56935943+mathiasm1@users.noreply.github.com>
2019-11-26 14:28:19 -0300

    agregado ejercicio formularios-hooks

---
+++ Proyectos/MathiasM/formularios-hooks/src/components/Function.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+
+import '../App.css';
+const genders = ['male', 'female', 'other'];
+
+const Function = () =>  {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [gender, setGender] = useState('');
+  const [birthdate, setBirthdate] = useState('');
+
+    
+  const handleChange = (key) => ({ target: { value }}) => {
+    switch (key) {
+        case 'firstName':
+          setFirstName(value)
+          //console.log('setFirstName', value)
+          break;
+        case 'lastName':
+          setLastName(value)
+          break;
+        case 'email':
+          setEmail(value)
+          break;
+        case 'gender':
+          setGender(value)
+          break;
+        case 'birthdate':
+          setBirthdate(value)
+          break;
+      default:
+        break;
+    }
+  }
+
+  const handleSubmit = () => {
+    alert(`${firstName} ${lastName} ${email} ${gender} ${birthdate}`)
+  }
+
+  const renderGendersMap = () => {
+    return genders.map((gender) => {
+      return (
+        <div key={gender}>
+          <input 
+            className='App-Form-Item' 
+            name='genders' 
+            required 
+            type='radio' 
+            value={gender}
+            onChange={handleChange('gender')} 
+            />{gender}
+        </div>
+      );
+    })
+  }
+
+  return (
+    <div className='App'>
+      <form className='App-Form' onSubmit={handleSubmit}>
+        <input className='App-Form-Item' required value={firstName} onChange={handleChange('firstName')} />
+        <input className='App-Form-Item' required value={lastName} onChange={handleChange('lastName')} />
+        <input className='App-Form-Item' required type='email' value={email} onChange={handleChange('email')} />
+        {renderGendersMap()}
+        <input className='App-Form-Item' required type='date' value={birthdate} onChange={handleChange('birthdate')} />
+        <button className='App-Form-Item' type='submit'>{'Submit'}</button>
+      </form>
+    </div>
+  );
+}
+
+export default Function;
