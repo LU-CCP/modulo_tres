17b413484110c451d8f237cc570e155ae95f95cf
mathiasm-lagash <56935943+mathiasm1@users.noreply.github.com>
2019-11-27 15:56:03 -0300

    update ejercicio redux agregado store

---
+++ Proyectos/MathiasM/redux/src/native/reducers/counter.js
@@ -0,0 +1,14 @@
+const INITIAL_STATE = {
+  count: 0,
+  isIncrementing: false,
+  isDecrementing: false
+};
+
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
