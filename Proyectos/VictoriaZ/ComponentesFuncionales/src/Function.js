707fd0e004f0374b40be6e57e488ab73692f9dab
Victoria Zanforlin <victoriaz@lagash.com>
2019-11-27 13:41:10 -0300

    Se instalan paquetes para eslint y se copian los archivos correspondientes

--- Proyectos/VictoriaZ/ComponentesFuncionales/src/Function.js
+++ Proyectos/VictoriaZ/ComponentesFuncionales/src/Function.js
@@ -1,53 +1,75 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 
 const genders = ['male', 'female', 'other'];
 
-const Function =()=>{
-    const[firstName, setFirstName] =useState('');
-    const[lastName, setLastName] =useState('');
-    const[email, setEmail] =useState('');
-    const[gender, setGender] =useState('');
-    const[birthdate, setBirthdate] =useState('');
+const Function = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [gender, setGender] = useState('');
+  const [birthdate, setBirthdate] = useState('');
 
-    const handleChange = (key) => ({ target: { value }}) => {
+  const handleChange = key => ({ target: { value } }) => {};
 
-      }
-    
-    const handleSubmit = () => {
-        const { firstName, lastName, email, gender, birthdate } = state;
-        alert(`${firstName} ${lastName} ${email} ${gender} ${birthdate}`)
-      }
-    
+  const handleSubmit = () => {
+    const { firstName, lastName, email, gender, birthdate } = state;
 
-    const renderGendersMap = () => {
-        return genders.map((gender) => {
-          return (
-            <div key={gender}>
-              <input 
-                className='App-Form-Item' 
-                name='genders' 
-                required 
-                type='radio' 
-                value={gender}
-                onChange={handleChange('gender')} 
-                />{gender}
-            </div>
-          );
-        })
-      }
+    alert(`${firstName} ${lastName} ${email} ${gender} ${birthdate}`);
+  };
 
-    return ( //return de la funcion es equivalente al render
-        <div className='App'>
-          <form className='App-Form' onSubmit={handleSubmit}>
-            <input className='App-Form-Item' required value={firstName} onChange={handleChange('firstName')} />
-            <input className='App-Form-Item' required value={lastName} onChange={handleChange('lastName')} />
-            <input className='App-Form-Item' required type='email' value={email} onChange={handleChange('email')} />
-            {renderGendersMap()}
-            <input className='App-Form-Item' required type='date' value={birthdate} onChange={handleChange('birthdate')} />
-            <button className='App-Form-Item' type='submit'>{'Submit'}</button>
-          </form>
+  const renderGendersMap = () => genders.map(gender => {
+      return (
+        <div key={gender}>
+          <input
+            className='App-Form-Item'
+            name='genders'
+            required
+            type='radio'
+            value={gender}
+            onChange={handleChange('gender')}
+          />
+          {gender}
         </div>
       );
-}
+    });
+
+  return (
+    // return de la funcion es equivalente al render
+    <div className='App'>
+      <form className='App-Form' onSubmit={handleSubmit}>
+        <input
+          className='App-Form-Item'
+          required
+          value={firstName}
+          onChange={handleChange('firstName')}
+        />
+        <input
+          className='App-Form-Item'
+          required
+          value={lastName}
+          onChange={handleChange('lastName')}
+        />
+        <input
+          className='App-Form-Item'
+          required
+          type='email'
+          value={email}
+          onChange={handleChange('email')}
+        />
+        {renderGendersMap()}
+        <input
+          className='App-Form-Item'
+          required
+          type='date'
+          value={birthdate}
+          onChange={handleChange('birthdate')}
+        />
+        <button className='App-Form-Item' type='submit'>
+          {'Submit'}
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Function;
