3c03151ffb7b7db81c2e33984ef14845e2be4177
melinag <melinag@lagash.com>
2019-11-27 14:01:48 -0300

    implementacion de slint  en HyperlinkHooks.js

--- Proyectos/melina-gomez/ciclo-de-vida-estado-propiedades/src/HyperlinkHook.js
+++ Proyectos/melina-gomez/ciclo-de-vida-estado-propiedades/src/HyperlinkHook.js
@@ -1,50 +1,52 @@
-import React, {memo,useRef,useState,useEffect} from 'react';
-const Hyperlink = (props) => {
-    const [value, setValue] = useState ('Learn React');
-    const [compare, setCompare] =useState('')
-    const [showInstructions,setShowInstruccions] = useState(true)
-    const _varGlobalRef = useRef('');
+import React, { memo, useRef, useState, useEffect } from 'react';
 
-    useEffect(() => {
-        if (showInstructions !== props.showInstructions) {
-           setShowInstruccions(props.showInstructions)
-    } },[props.showInstructions]
-    )
-    useEffect(()=>{
-        console.log('Hyperlink componentDidMount')
+const Hyperlink = props => {
+  const [value, setValue] = useState('Learn React');
+  const [setCompare] = useState('');
+  const [showInstructions, setShowInstruccions] = useState(true);
+  const _varGlobalRef = useRef('');
+
+  useEffect(() => {
+    if (showInstructions !== props.showInstructions) {
+      setShowInstruccions(props.showInstructions);
+    }
+  }, [props.showInstructions, showInstructions]);
+  useEffect(() => {
+    console.log('Hyperlink componentDidMount');
     setTimeout(() => {
-      console.log('Hyperlink setState')
+      console.log('Hyperlink setState');
       setValue('Learn React Now!');
-    }, 3000)
+    }, 3000);
     setTimeout(() => {
       _varGlobalRef.current = 'Please';
-      console.log('Hyperlink forceUpdate')
-    }, 4000)
+      console.log('Hyperlink forceUpdate');
+    }, 4000);
     setTimeout(() => {
-      console.log('Hyperlink setState compare')
+      console.log('Hyperlink setState compare');
       setCompare('');
-      const { onHide }  = props;
+      const { onHide } = props;
+
       onHide();
-    }, 5000)
-  },[])
-   useEffect(() => {
-       console.log('HyperLink componentUpdate')
-   })
-   console.log('Hyperlink render')
+    }, 5000);
+  }, [props, setCompare]);
+  useEffect(() => {
+    console.log('HyperLink componentUpdate');
+  });
+  console.log('Hyperlink render');
 
-    return (
-      <div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {`${value} ${_varGlobalRef.current}`}
-        </a>
-        {!showInstructions && <p>Can you see the instructions?</p>}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <a
+        className='App-link'
+        href='https://reactjs.org'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        {`${value} ${_varGlobalRef.current}`}
+      </a>
+      {!showInstructions && <p>Can you see the instructions?</p>}
+    </div>
+  );
+};
 
-export default memo(Hyperlink)
+export default memo(Hyperlink);
