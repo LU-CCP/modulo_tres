9d96a2c44b8f9e441ab1fcb9d6664ad848f68ff6
Vicenteh <57122161+Vicenteh@users.noreply.github.com>
2019-11-27 15:09:27 -0300

    Eslinteos dia 26/11

--- Proyectos/VicenteH/ciclo-de-vida-hooks/src/appHook.js
+++ Proyectos/VicenteH/ciclo-de-vida-hooks/src/appHook.js
@@ -1,52 +1,54 @@
-import React, {memo, useState, useRef, useEffect } from 'react';
+import React, { memo, useState, useRef, useEffect } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-//const arr = [1, 2];
-//const [uno,dos ] = arr;
+// const arr = [1, 2];
+// const [uno,dos ] = arr;
 
 const App = () => {
-    const _varGlobalRef = useRef('');
-    const [value , setValue] = useState('Learn React')
-    const [compare , setCompare] = useState('Learn React')
+  const _varGlobalRef = useRef('');
+  const [value, setValue] = useState('Learn React');
+  const [compare, setCompare] = useState('Learn React');
 
-    //ComponentDidMount
-    useEffect(() => {
-        console.log('componentDidMount')
-        setTimeout(() => {
-          console.log('setState')
-          setValue('Learn React Now!');
-        }, 3000)
-        setTimeout(() => {
-          _varGlobalRef.current = 'Please';
-        }, 4000)
-        setTimeout(() => {
-          console.log('setState compare')
-          setCompare('');
-        }, 5000)
-    }, []);
-    //ComponentDidUpdate
-    useEffect(() => {
-        console.log('componentDidUpdate', value)
-    },[value]);
-    return(
-        <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {`${value} ${_varGlobalRef.current}`}
-          </a>
-        </header>
-      </div>
-    )
-}
+  // ComponentDidMount
+  useEffect(() => {
+    console.log('componentDidMount');
+    setTimeout(() => {
+      console.log('setState');
+      setValue('Learn React Now!');
+    }, 3000);
+    setTimeout(() => {
+      _varGlobalRef.current = 'Please';
+    }, 4000);
+    setTimeout(() => {
+      console.log('setState compare');
+      setCompare('');
+    }, 5000);
+  }, []);
+  // ComponentDidUpdate
+  useEffect(() => {
+    console.log('componentDidUpdate', value);
+  }, [value]);
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          {`${value} ${_varGlobalRef.current}`}
+        </a>
+      </header>
+    </div>
+  );
+};
 
 export default memo(App);
