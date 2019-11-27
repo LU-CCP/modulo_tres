04f70c116b2c7961b0b9df4d6eff538eba1de3c4
mathiasm-lagash <56935943+mathiasm1@users.noreply.github.com>
2019-11-27 13:45:37 -0300

    agregado correcion en ejercicio componentes-funcionales con lint

--- Proyectos/MathiasM/Componentes-Funcionales/src/components/Functions.js
+++ Proyectos/MathiasM/Componentes-Funcionales/src/components/Functions.js
@@ -2,50 +2,72 @@ import React, { Component, useState } from 'react';
 
 const genders = ['male', 'female', 'other'];
 
-const Function = ()=>{
-    const [firstName, setFirstName] = setState();
-    const [lastName, setLastName] = setState();
-    const [email, setEmail] = setState();
-    const [gender, setGender] = setState();
-    const [birthdate, setBirthdate] = setState();
+const Function = () => {
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [gender, setGender] = useState();
+  const [birthdate, setBirthdate] = useState();
 
-    const handleChange = (key) => ({ target: { value }}) => {
-    }
-    
-    const handleSubmit = () => {
-        const { firstName, lastName, email, gender, birthdate } = state;
-        alert(`${firstName} ${lastName} ${email} ${gender} ${birthdate}`)
-    }
+  const handleChange = key => ({ target: { value } }) => {};
 
-    const renderGendersMap = () => {
-        return genders.map((gender) => {
-          return (
-            <div key={gender}>
-              <input 
-                className='App-Form-Item' 
-                name='genders' 
-                required 
-                type='radio' 
-                value={gender}
-                onChange={handleChange('gender')} 
-                />{gender}
-            </div>
-          );
-        })
-      }
+  const handleSubmit = () => {
+    const { firstName, lastName, email, gender, birthdate } = useState;
 
-    return (
-        <div className='App'>
-          <form className='App-Form' onSubmit={handleSubmit}>
-            <input className='App-Form-Item' required value={firstName} onChange={handleChange('firstName')} />
-            <input className='App-Form-Item' required value={lastName} onChange={handleChange('lastName')} />
-            <input className='App-Form-Item' required type='email' value={email} onChange={handleChange('email')} />
-            {renderGendersMap()}
-            <input className='App-Form-Item' required type='date' value={birthdate} onChange={handleChange('birthdate')} />
-            <button className='App-Form-Item' type='submit'>{'Submit'}</button>
-          </form>
-        </div>
-      );
-}
+    alert(`${firstName} ${lastName} ${email} ${gender} ${birthdate}`);
+  };
+
+  const renderGendersMap = () =>
+    genders.map(gender => (
+      <div key={gender}>
+        <input
+          className='App-Form-Item'
+          name='genders'
+          required
+          type='radio'
+          value={gender}
+          onChange={handleChange('gender')}
+        />
+        {gender}
+      </div>
+    ));
+
+  return (
+    <div className='App'>
+      <form className='App-Form' onSubmit={handleSubmit}>
+        <input
+          className='App-Form-Item'
+          required
+          value={firstName}
+          onChange={handleChange('firstName')}
+        />
+        <input
+          className='App-Form-Item'
+          required
+          value={lastName}
+          onChange={handleChange('lastName')}
+        />
+        <input
+          className='App-Form-Item'
+          required
+          type='email'
+          value={email}
+          onChange={handleChange('email')}
+        />
+        {renderGendersMap()}
+        <input
+          className='App-Form-Item'
+          required
+          type='date'
+          value={birthdate}
+          onChange={handleChange('birthdate')}
+        />
+        <button className='App-Form-Item' type='submit'>
+          {'Submit'}
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Function;
