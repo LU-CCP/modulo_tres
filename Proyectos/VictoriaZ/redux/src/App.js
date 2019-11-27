62f0fde1164e6e067487cd9144f85acf51db4aea
Victoria Zanforlin <victoriaz@lagash.com>
2019-11-27 17:40:14 -0300

    Se importa counter a App.js

--- Proyectos/VictoriaZ/redux/src/App.js
+++ Proyectos/VictoriaZ/redux/src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 
 import configureStore from './store';
+import Counter from './Counter';
 
 const { store } = configureStore();
 
-const App = () => <Provider store={store} />;
+const App = () => (
+  <Provider store={store}>
+    <Counter />
+  </Provider>
+);
 
 export default App;
