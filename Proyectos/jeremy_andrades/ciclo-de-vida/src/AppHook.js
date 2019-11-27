6631bd3f9ef2b8fedbce6852358eb8a990d7283c
jeremya-lagash <jeremya@lagash.com>
2019-11-27 00:01:47 -0300

    casi arreglado el problema de npm

--- Proyectos/jeremy_andrades/ciclo-de-vida/src/AppHook.js
+++ Proyectos/jeremy_andrades/ciclo-de-vida/src/AppHook.js
@@ -1,50 +1,49 @@
-import React, { useEffect, useState, useRef, memo } from 'react';
+import React, { useEffect, useState, useRef, memo } from "react";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 // const arr = [1,2];
 // const [uno, dos] = arr;
 
 const App = () => {
-  const _varGlobalRef = useRef('');
-  const [value, setValue] = useState('Learn React');
-  // const [compare, setCompare] = useState('');
-  const [setCompare] = useState('');
+  const _varGlobalRef = useRef("");
+  const [value, setValue] = useState("Learn React");
+  const [compare, setCompare] = useState("");
 
   // componentDidMount
   useEffect(() => {
-    console.log('componentDidMount');
+    console.log("componentDidMount");
     setTimeout(() => {
-      console.log('setState');
-      setValue('Learn React Now!');
+      console.log("setState");
+      setValue("Learn React Now!");
     }, 3000);
     setTimeout(() => {
-      _varGlobalRef.current = 'Please';
+      _varGlobalRef.current = "Please";
     }, 4000);
     setTimeout(() => {
-      console.log('setState compare');
-      setCompare('');
+      console.log(compare);
+      setCompare("");
     }, 5000);
-  }, [setCompare]);
+  }, [compare, setCompare]);
 
   // componentDidUpdate solo cuando value cambia
   useEffect(() => {
-    console.log('componentDidUpdate value', value);
+    console.log("componentDidUpdate value", value);
   }, [value]);
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           {`${value} ${_varGlobalRef.current}`}
         </a>
