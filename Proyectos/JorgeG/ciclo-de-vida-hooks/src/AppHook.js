a29d017f384777467dddb8c2f60eb74ac13d55f3
jorgege-lagash <57356804+jorgege-lagash@users.noreply.github.com>
2019-11-27 13:58:03 -0300

    correcciones de lint

--- Proyectos/JorgeG/ciclo-de-vida-hooks/src/AppHook.js
+++ Proyectos/JorgeG/ciclo-de-vida-hooks/src/AppHook.js
@@ -1,56 +1,57 @@
 import React, { memo, useEffect, useState, useRef } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-const App = () =>{
-const _varGlobalRef = useRef('');
-const [value, setValue] = useState('Learn React')
-const [compare, setCompare] = useState('Learn React')
+const App = () => {
+  const _varGlobalRef = useRef('');
+  const [value, setValue] = useState('Learn React');
+  const [compare, setCompare] = useState('Learn React');
 
-    // useEffect es equivalente a componentDidMount()
-    useEffect(() =>{
-        console.log('componentDidMount')
-        setTimeout(() => {
-          console.log('setState')
-          setValue('Learn React Now!');
-        }, 3000)
-        setTimeout(() => {
-          _varGlobalRef.current = 'Please';
-        }, 4000)
-        setTimeout(() => {
-          console.log('setState compare')
-          setCompare('')
-        }, 5000)
-    },[]);
+  // useEffect es equivalente a componentDidMount()
+  useEffect(() => {
+    console.log('componentDidMount');
+    setTimeout(() => {
+      console.log('setState');
+      setValue('Learn React Now!');
+    }, 3000);
+    setTimeout(() => {
+      _varGlobalRef.current = 'Please';
+    }, 4000);
+    setTimeout(() => {
+      console.log('setState compare');
+      setCompare('');
+    }, 5000);
+  }, []);
 
-    // este es casi igual a componentDidUpdate() 
-    useEffect(() =>{
-        console.log('componentDidUpdate')
-    });
+  // este es casi igual a componentDidUpdate()
+  useEffect(() => {
+    console.log('componentDidUpdate');
+  });
 
-    // este es casi igual a componentDidUpdate() 
-    useEffect(() =>{
-        console.log('componentDidUpdate value')
-    },[value]);
+  // este es casi igual a componentDidUpdate()
+  useEffect(() => {
+    console.log('componentDidUpdate value');
+  }, [value]);
 
-    return(
-        <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {`${value} ${_varGlobalRef.current}`}
-          </a>
-        </header>
-      </div>
-    )
-}
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          {`${value} ${_varGlobalRef.current}`}
+        </a>
+      </header>
+    </div>
+  );
+};
 
 export default memo(App);
