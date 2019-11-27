3de7d1e4eca98ba1b067ff14b6ff4223dafabe40
bryanm-lagash <bryanm@lagash.com>
2019-11-27 13:44:41 -0300

    Algunos cambios realizados

--- Proyectos/BryanM/formularios-hooks/src/components/ReactHookForm.js
+++ Proyectos/BryanM/formularios-hooks/src/components/ReactHookForm.js
@@ -1,25 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import useForm from 'react-hook-form'
 import '../App.css';
 
 const Function = () => {
-    const {register, handleSubmit} = useForm()
+    const {register, handleSubmit } = useForm()
 
-    const onSubmit = () => {
+    const onSubmit = (data) => {
         const { firstName, lastName, email, birthdate } = data;
         alert(`${firstName} ${lastName} ${email} ${birthdate}`)
+        console.log(data);
     }
 
     return (
         <div className='App'>
-            <form className='App-Form' onSubmit={onSubmit}>
-              <input className='App-Form-Item' required name='firstName' ref={register} />
-              <input className='App-Form-Item' required name='lastName' ref={register}/>
-              <input className='App-Form-Item' required type='email' name='email' ref={register}/>
-              <input className='App-Form-Item' required type='date' name='birthdate' ref={register}/>
-              <button className='App-Form-Item' type='submit'>{'Submit'}</button>
+            <form className='App-Form' onSubmit={handleSubmit(onSubmit)}>
+                <input className='App-Form-Item' name='firstName' ref={register({ required:true })} />
+                <input className='App-Form-Item' required name='lastName' ref={register}/>
+                <input className='App-Form-Item' required type='email' name='email' ref={register}/>
+                <input className='App-Form-Item' required type='date' name='birthdate' ref={register}/>
+                <button className='App-Form-Item' type='submit'>{'Submit'}</button>
             </form>
-          </div>
+        </div>
       );
 }
 
