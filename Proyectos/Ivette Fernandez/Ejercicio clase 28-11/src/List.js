559a7e17bc4b939a87cd34eee3335e7cd109b77d
ivettef <56972785+ivettef@users.noreply.github.com>
2019-11-28 18:11:08 -0300

    avance de  ejercicio en clase

--- Proyectos/Ivette Fernandez/Ejercicio clase 28-11/src/List.js
+++ Proyectos/Ivette Fernandez/Ejercicio clase 28-11/src/List.js
@@ -1,3 +1,41 @@
-const List = () => null;
+import React from 'react';
+
+import { sendLikes, sendDislikes } from './actions/list';
+
+const List = () => (
+  <div
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+    }}
+  >
+    <h1>MENÚ</h1>
+    <p>Like Count: {sendLikes}</p>
+    <p>Dislike Count: {sendDislikes}</p>
+    <div
+      style={{
+        width: 1000,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+      }}
+    >
+      {/* <button onClick={onIncrement}>Increment</button>
+        <button onClick={onIncrementAsync} disabled={isIncrementing}>
+          Increment Async
+        </button> */}
+      {/* <div>{likeMenus.map(renderItem)}</div>
+      <div>{menus.map(renderMenu)}</div>
+      <div>{dislikeMenus.map(renderItem)}</div> */}
+    </div>
+    {/* <div>
+        <button onClick={onDecrement}>Decrement</button>
+        <button onClick={onDecrementAsync} disabled={isDecrementing}>
+          Decrement Async
+        </button>
+      </div> */}
+  </div>
+);
 
 export default List;
--- Proyectos/Ivette Fernandez/Ejercicio clase 28-11/src/reducers/list.js
+++ Proyectos/Ivette Fernandez/Ejercicio clase 28-11/src/reducers/list.js
@@ -1,7 +1,31 @@
 import { createReducer } from 'reduxsauce';
+import produce from 'immer';
 
-const INITIAL_STATE = {};
+import { SEND_LIKES, SEND_DISLIKES } from '../actions/list';
 
-const reducer = createReducer(INITIAL_STATE, {});
+const INITIAL_STATE = {
+  menus: ['uno', 'dos', 'tres', 'cuatro'],
+  likeMenus: [],
+  dislikesMenus: [],
+  likes: 0,
+  dislikes: 0
+};
+
+const Likes = produce((draft, { index }) => {
+  draft.likes += 1;
+  draft.likeMenus.push(draft.menus[index]);
+  draft.menus.slice(0, 1);
+});
+
+const Dislikes = produce((draft, { index }) => {
+  draft.dislikes += 1;
+  draft.dislikeMenus.push(draft.menus[index]);
+  draft.menus.slice(0, 1);
+});
+
+const reducer = createReducer(INITIAL_STATE, {
+  [SEND_LIKES]: Likes,
+  [SEND_DISLIKES]: Dislikes
+});
 
 export default reducer;
