7c588040598ca16e1c6f9405332a2a97735ee748
estebanl-lagash <estebanl@lagash.com>
2019-11-29 14:17:52 -0300

    actualizar

--- Base/ciclo-de-vida-Hooks/src/AppHook.js
+++ Base/ciclo-de-vida-Hooks/src/AppHook.js
@@ -1,5 +1,4 @@
 import React, { memo, useEffect, useState, useRef } from 'react';
-
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,55 +6,56 @@ import './App.css';
 // const [uno, dos] = arr;
 
 const App = () => {
-  const _varGlobalRef = useRef('');
-  const [value, setValue] = useState('Learn React');
-  const [setCompare] = useState('');
+    const _varGlobalRef = useRef('');
+    const [value, setValue] = useState('Learn React')
+    const [compare, setCompare] = useState('')
 
-  // componentDidMount
-  useEffect(() => {
-    console.log('componentDidMount');
-    setTimeout(() => {
-      console.log('setState');
-      setValue('Learn React Now!');
-    }, 3000);
-    setTimeout(() => {
-      _varGlobalRef.current = 'Please';
-    }, 4000);
-    setTimeout(() => {
-      console.log('setState compare');
-      setCompare('');
-    }, 5000);
-  }, [setCompare]);
+    // componentDidMount
+    useEffect(() => {
+        console.log('componentDidMount')
+        setTimeout(() => {
+          console.log('setState')
+          setValue( 'Learn React Now!');
+        }, 3000)
+        setTimeout(() => {
+          _varGlobalRef.current = 'Please';
+        }, 4000)
+        setTimeout(() => {
+          console.log('setState compare')
+          setCompare('');
+        }, 5000)
+    }, []);
 
-  // componentDidUpdate
-  useEffect(() => {
-    console.log('componentDidUpdate');
-  });
+    // componentDidUpdate
+    useEffect(() => {
+        console.log('componentDidUpdate')
+    });
 
-  // componentDidUpdate solo cuando value cambia
-  useEffect(() => {
-    console.log('componentDidUpdate value', value);
-  }, [value]);
+    
+    // componentDidUpdate solo cuando value cambia
+    useEffect(() => {
+        console.log('componentDidUpdate value', value)
+    },[value]);
 
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          {`${value} ${_varGlobalRef.current}`}
-        </a>
-      </header>
+    return (
+      <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+            Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            {`${value} ${_varGlobalRef.current}`}
+            </a>
+        </header>
     </div>
-  );
-};
+    )
+}
 
 // equivalente a shouldComponentUpdate y PureComponent
 export default memo(App);
---
+++ Base/redux-router/src/containers/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+    <button>About</button>
+    <button>Counter</button>
+  </div>
+);
+
+export default Home;
