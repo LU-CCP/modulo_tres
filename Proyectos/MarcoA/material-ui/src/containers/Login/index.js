becf1de5d763739bf42a70289f2faec8126fd03a
marcoac001 <marcoac001@lagash.com>
2019-11-29 16:52:27 -0300

    Se agrega un nuevo Login pero con material UI

--- Proyectos/MarcoA/material-ui/src/containers/Login/index.js
+++ Proyectos/MarcoA/material-ui/src/containers/Login/index.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import { Button, Container, Grid, Paper, TextField } from '@material-ui/core';
 
-const Home = () => (
-  <div
-    style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center'
-    }}
-  >
-    <p>User</p>
-    <input />
-    <p>Password</p>
-    <input type='password' />
-    <button>Login</button>
-  </div>
+const Login = () => (
+  <Container maxWidth={false}>
+    Login Lagash
+    <Grid>
+      <Paper>
+        <TextField variant='outlined' label='Usuario' />
+        <TextField variant='outlined' label='Contraseña' type='password' />
+        <Button variant='outlined'>Login</Button>
+      </Paper>
+    </Grid>
+  </Container>
 );
+// const Login = () => (
+//   <div
+//     style={{
+//       display: 'flex',
+//       flexDirection: 'column',
+//       alignItems: 'center'
+//     }}
+//   >
+//     <p>User</p>
+//     <input />
+//     <p>Password</p>
+//     <input type='password' />
+//     <button>Login</button>
+//   </div>
+// );
 
-export default Home;
+export default Login;
