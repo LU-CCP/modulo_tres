60717bcfa7b180e8f6d2ce75db209efbbc8181b0
fabianb-lagash <fabianb@lagash.com>
2019-11-29 16:23:06 -0300

    se agregan modificaciones al proyecto

--- Proyectos/FabianB/redux-router/src/containers/About.js
+++ Proyectos/FabianB/redux-router/src/containers/About.js
@@ -1,17 +1,34 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { goBack } from 'connected-react-router';
 
+import useMount from '../hook/useMount';
+import jsonApi from '../services/jsonApi';
+
 const About = () => {
   const dispatch = useDispatch();
+  const [user, setUsers] = useState([]);
 
   const handleGoBack = useCallback(() => dispatch(goBack()), [dispatch]);
+
+  useMount(async () => {
+    const { data } = await jsonApi().getUsers();
+
+    if (Array.isArray(data)) {
+      setUsers(data);
+    }
+  });
 
   return (
     <div>
       <h1>About Page</h1>
       <p>Did you get here via Redux?</p>
       <button onClick={handleGoBack}>Go Back</button>
+      <div>
+        {user.map(({ id, name }) => (
+          <p key={id}>{name}</p>
+        ))}
+      </div>
     </div>
   );
 };
