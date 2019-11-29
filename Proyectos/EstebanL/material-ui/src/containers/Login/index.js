7953a077fa602679db79e1819906ebc991bd887e
estebanl-lagash <estebanl@lagash.com>
2019-11-29 17:46:43 -0300

    Practica CSS en react

---
+++ Proyectos/EstebanL/material-ui/src/containers/Login/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Button, Container, Grid, Paper, TextField } from '@material-ui/core';
+
+import useStyles from './styles';
+
+const Login = () => {
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.container} maxWidth={false}>
+      <Grid className={classes.grid}>
+        <Paper className={classes.paper}>
+          <TextField className={classes.item} variant='outlined' label='User' />
+          <TextField
+            className={classes.item}
+            variant='outlined'
+            label='Password'
+            type='password'
+          />
+          <Button className={classes.item} variante='outlined'>
+            Login
+          </Button>
+        </Paper>
+      </Grid>
+    </Container>
+  );
+};
+
+// const Login = () => (
+//   <div
+//     style={{
+//       display: 'flex',
+//       flexDirection: 'column',
+//       alignItems: 'center'
+//     }}
+//   >
+//     <p>User</p>
+//     <input />
+//     <p>Password</p>
+//     <input type='password' />
+//     <button>Login</button>
+//   </div>
+// );
+
+export default Login;
