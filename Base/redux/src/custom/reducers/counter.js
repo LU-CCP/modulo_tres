f2017c4bcc783290d613a3e624e87612084cab80
Alejandro <alejandroce@lagash.com>
2019-11-28 21:47:55 -0300

    update redux project

--- Base/redux/src/custom/reducers/counter.js
+++ Base/redux/src/custom/reducers/counter.js
@@ -15,6 +15,7 @@ const INITIAL_STATE = {
 };
 
 const increment = produce(draft => {
+  // draft.count = draft.count += 1;
   draft.count += 1;
   draft.isIncrementing = false;
 });
@@ -23,21 +24,21 @@ const incrementAsync = produce(draft => {
   draft.isIncrementing = true;
 });
 
-const decremnent = produce(draft => {
+const decrement = produce(draft => {
   // draft.count = draft.count - 1;
   draft.count -= 1;
   draft.isDecrementing = false;
 });
 
-const decremnentAsync = produce(draft => {
+const decrementAsync = produce(draft => {
   draft.isDecrementing = true;
 });
 
 const reducer = createReducer(INITIAL_STATE, {
   [INCREMENT]: increment,
   [INCREMENT_ASYNC]: incrementAsync,
-  [DECREMENT]: decremnent,
-  [DECREMENT_ASYNC]: decremnentAsync
+  [DECREMENT]: decrement,
+  [DECREMENT_ASYNC]: decrementAsync
 });
 
 export default reducer;
