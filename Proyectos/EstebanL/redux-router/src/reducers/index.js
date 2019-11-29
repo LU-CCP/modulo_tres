624e1cfde08f37b2b1df2a867cec5867374c157d
estebanl-lagash <estebanl@lagash.com>
2019-11-29 16:22:56 -0300

    Actualizacion y finalizacion ejemplo de paginas

--- Proyectos/EstebanL/redux-router/src/reducers/index.js
+++ Proyectos/EstebanL/redux-router/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
 
 import counter from './counter';
 
-function createRootReducer() {
-  const rootReducer = combineReducers({ counter });
+function createRootReducer(history) {
+  const router = connectRouter(history);
+  const rootReducer = combineReducers({ router, counter });
 
   return rootReducer;
 }
---
+++ Proyectos/EstebanL/redux-router/src/services/jsonApi.js
@@ -0,0 +1,17 @@
+import { create } from 'apisauce';
+
+const config = {
+  baseURL: 'https://jsonplaceholder.typicode.com'
+};
+
+const createApi = () => {
+  const { get } = create(config);
+
+  const getUsers = () => get('users');
+
+  return {
+    getUsers
+  };
+};
+
+export default createApi;
