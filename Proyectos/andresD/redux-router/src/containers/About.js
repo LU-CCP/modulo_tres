6c050446717dbddfa2192365599ac77dfad88e57
andresd-lagash <57415589+andresd-lagash@users.noreply.github.com>
2019-11-29 16:31:36 -0300

    toute

---
+++ Proyectos/andresD/redux-router/src/containers/About.js
@@ -0,0 +1,35 @@
+import React, { useCallback, useState } from 'react';
+import { goBack } from 'connected-react-router';
+import { useDispatch } from 'react-redux';
+
+import useMount from '../hooks/useMount';
+import jsonApi from '../services/jsonApi';
+
+const About = () => {
+  const dispatch = useDispatch();
+  const [users, setUsers] = useState([]);
+  const handleGoBack = useCallback(() => dispatch(goBack()), [dispatch]);
+
+  useMount(async () => {
+    const { data } = await jsonApi().getUsers();
+
+    if (Array.isArray(data)) {
+      setUsers(data);
+    }
+  });
+
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Did you get here via Redux?</p>
+      <button onClick={handleGoBack}>Go Back</button>
+      <div>
+        {users.map(({ id, name }) => (
+          <p key={id}>{name}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ Proyectos/andresD/redux-router/src/containers/Home.js
@@ -0,0 +1,30 @@
+import React, { useCallback } from 'react';
+import { Link } from 'react-router-dom';
+import { push } from 'connected-react-router';
+import { useDispatch } from 'react-redux';
+
+import { ABOUT, COUNTER } from '../routes/paths';
+
+const Home = () => {
+  const dispatch = useDispatch();
+
+  const handleNavigate = useCallback(path => () => dispatch(push(path)), [
+    dispatch
+  ]);
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <div>
+        <Link to={ABOUT}>About</Link>
+      </div>
+      <div>
+        <Link to={COUNTER}>Counter</Link>
+      </div>
+      <button onClick={handleNavigate(ABOUT)}>About</button>
+      <button onClick={handleNavigate(COUNTER)}>Counter</button>
+    </div>
+  );
+};
+
+export default Home;
