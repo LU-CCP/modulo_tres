69da471004dfdf04c771091b2b15bb20553c41d2
Alejandro <alejandroce@lagash.com>
2019-12-12 14:43:31 -0300

    project jemery y felipe

--- Proyectos/FelipeP/trabajo-final-react/src/actions/users.js
+++ Proyectos/FelipeP/trabajo-final-react/src/actions/users.js
@@ -4,22 +4,25 @@ const { Types, Creators } = createActions(
   {
     saveUser: ['user'],
     deleteUser: ['index'],
-    updateUser: ['user', 'index']
+    updateUser: ['user', 'index'],
+    setUserEdit: ['index', 'data']
   },
   {
     prefix: 'USER/'
   }
 );
 
-const { saveUser, deleteUser, updateUser } = Creators;
+const { saveUser, deleteUser, updateUser, setUserEdit } = Creators;
 
-const { SAVE_USER, DELETE_USER, UPDATE_USER } = Types;
+const { SAVE_USER, DELETE_USER, UPDATE_USER, SET_USER_EDIT } = Types;
 
 export {
   saveUser,
   SAVE_USER,
   deleteUser,
+  setUserEdit,
   DELETE_USER,
   updateUser,
-  UPDATE_USER
+  UPDATE_USER,
+  SET_USER_EDIT
 };
