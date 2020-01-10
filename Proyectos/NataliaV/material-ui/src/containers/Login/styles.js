86c3cb4674e3a28236ee0ec83e45b65e4d9672eb
nataliav-lagash <nataliav@lagash.com>
2020-01-10 16:10:20 -0300

    avance

--- Proyectos/NataliaV/material-ui/src/containers/Login/styles.js
+++ Proyectos/NataliaV/material-ui/src/containers/Login/styles.js
@@ -5,26 +5,26 @@ const useStyles = makeStyles(theme => ({
     paddingLeft: 0,
     paddingRight: 0
   },
-  /* grid: {
-    // display: 'flex',
-    // height: '100vh',
+  grid: {
+    display: 'flex',
+    height: '100vh',
     alignItems: 'center',
     justifyContent: 'center',
-    // backgroundImage: `url(${loginBg}) `,
+    backgroundImage: `url(${loginBg}) `,
     [theme.breakpoints.down('md')]: {
       backgroundPosition: '25% 75%'
     },
     [theme.breakpoints.up('lg')]: {
       backgroundPosition: 'center'
     }
-  }, */
-  /* paper: {
+  },
+  paper: {
     display: 'flex',
     flexDirection: 'column',
     maxWidth: 300,
     padding: theme.spacing(4),
     opacity: 0.9
-  }, */
+  },
   item: {
     marginBottom: theme.spacing(2),
     marginTop: theme.spacing(2)
